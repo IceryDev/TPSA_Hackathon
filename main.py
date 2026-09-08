ecc0e6d8491309aba802878197be13b2a703a565
IceryDev <icerydev@protonmail.com>
2026-09-08 12:09:30 +0100

    Feat: Create Jupyter Notebook

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
+import os
+import sys
 
-print("Hello World")
+import matplotlib
+matplotlib.use("Qt5Agg")
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+import pypsa
+
+n = pypsa.Network("networks/WP2024_north-west.nc")
+
+
+print(n.buses.to_string())
+#n.plot(bus_sizes=0.0025, margin=0.25)
+#plt.show()
